5c0db4f428ca46a2c572d5fca0cdfbf2f457ab07
Saketh Velidimalla <svelidim@asu.edu>
2026-01-23 23:55:11 -0500

    Add resume PDF to repo and update download link

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -75,7 +75,7 @@ const Contact = () => {
               size="lg"
               variant="outline"
               className="border-primary/30 hover:bg-primary/10"
-              onClick={() => window.open('https://drive.google.com/file/d/1VkfAl10q6UTlopu-BTpCrDPq9xak_uaK/view?usp=sharing', '_blank')}
+              onClick={() => window.open(`${import.meta.env.BASE_URL}Saketh_Velidimalla_Resume.pdf`, '_blank')}
             >
               <FileText className="mr-2 h-5 w-5" />
               Download Resume
